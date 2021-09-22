43b2e38ad802b38cf0954531231c60a2bb0c22d2
dulvoker <80538157+dulvoker@users.noreply.github.com>
2021-09-22 19:46:21 +0600

    html and css

---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+const chatForm = document.getElementById('chat-form');
+const socket = io();
+
+socket.on('message', message => {
+    console.log(message);
+    outputMessage(message);
+})
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const msg = e.target.elements.msg.value; // To ask
+
+    socket.emit('chatMessage', msg);
+})
+
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class = "meta"> UserName <span> time </span></p>
+        <p class = "text">
+        ${message}
+        </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
