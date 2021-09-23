67afa381c570a83392afaf22ffedc0d5e54f37a5
dulvoker <80538157+dulvoker@users.noreply.github.com>
2021-09-23 17:12:24 +0600

    fixed outputFormattedMessage function

--- _html_css/js/main.js
+++ _html_css/js/main.js
@@ -3,13 +3,19 @@ const socket = io();
 
 socket.on('message', message => {
     console.log(message);
+    outputFormattedMessage(message);
 })
 
 // sending message
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
     const msg = e.target.elements.msg.value;
-
     socket.emit('messageSubmit', msg);
 });
+
+function outputFormattedMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta"> Dulat <span>time</span> </p> <p class="text">${message}</p>`
+    document.querySelector('.chat-messages').appendChild(div);
+}
