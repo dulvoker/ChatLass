410f5be739f597a7cb6d53c2809a8ca8317cae30
dulvoker <80538157+dulvoker@users.noreply.github.com>
2021-09-23 19:28:40 +0600

    messages with usernames and time

--- _html_css/js/main.js
+++ _html_css/js/main.js
@@ -15,11 +15,15 @@ chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const msg = e.target.elements.msg.value;
     socket.emit('messageSubmit', msg);
+
+    //clear input blank and focus on message
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
 });
 
 function outputFormattedMessage(message) {
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML = `<p class="meta"> Dulat <span>time</span> </p> <p class="text">${message}</p>`
+    div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span> </p> <p class="text">${message.text}</p>`
     document.querySelector('.chat-messages').appendChild(div);
 };
