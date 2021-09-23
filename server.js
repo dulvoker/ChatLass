7a7d0f92150dd1bd7d1e899d6e5cfaa542854189
dulvoker <80538157+dulvoker@users.noreply.github.com>
2021-09-23 15:43:39 +0600

    Welcoming new connection

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const path = require('path')
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
-
+const server = http.createServer(app);
+const io = socketio(server);
 //html and css
 app.use(express.static(path.join(__dirname, '_html_css')));
 
+//catching for connection
+io.on('connection', socket => {
+    console.log('Welcome');
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log('Server runs'));
+server.listen(PORT, () => console.log('Server runs'));
