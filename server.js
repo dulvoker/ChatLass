0e2ad5d78202b01a7d33379dba2ef7c2fd14d33d
dulvoker <80538157+dulvoker@users.noreply.github.com>
2021-09-23 15:58:30 +0600

    responsing for disconnection

--- server.js
+++ server.js
@@ -6,13 +6,24 @@ const socketio = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
 //html and css
 app.use(express.static(path.join(__dirname, '_html_css')));
 
-//catching for connection
+//events while connection
 io.on('connection', socket => {
-    console.log('Welcome');
+
+    //welcoming
+    socket.emit('message', 'Welcome to ChatLass');
+
+    //broadcasting to everyone except the user
+    socket.broadcast.emit('message', 'New user has joined the chat');
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    })
 });
+
 
 const PORT = 3000 || process.env.PORT;
 
