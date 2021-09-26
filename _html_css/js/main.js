e3610d9685ceb5f3be2b6b5c5514e3db3213dfd5
dulvoker <80538157+dulvoker@users.noreply.github.com>
2021-09-27 01:54:31 +0600

    new socket when joining the room

--- _html_css/js/main.js
+++ _html_css/js/main.js
@@ -6,7 +6,8 @@ const {username, room} = Qs.parse(location.search, {
     ignoreQueryPrefix:true,
 });
 
-console.log(username, room);
+//separating by Blocks
+socket.emit('joinRoom', {username, room});
 
 socket.on('message', message => {
     console.log(message);
